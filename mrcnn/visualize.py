dce4784af428a246d7e14e348f7c5f2e25aff337
Viarow <universe37@outlook.com>
2019-08-23 05:31:23 +0800

    Mask person class

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -69,7 +69,7 @@ def random_colors(N, bright=True):
     return colors
 
 
-def apply_mask(image, mask, color, alpha=0.5):
+def apply_mask(image, mask, color, alpha=1.0):
     """Apply the given mask to the image.
     """
     for c in range(3):
@@ -106,65 +106,73 @@ def display_instances(image, boxes, masks, class_ids, class_names,
 
     # If no axis is passed, create one and automatically call show()
     auto_show = False
-    if not ax:
-        _, ax = plt.subplots(1, figsize=figsize)
-        auto_show = True
+    # if not ax:
+    #     _, ax = plt.subplots(1, figsize=figsize)
+    #     auto_show = True
 
     # Generate random colors
-    colors = colors or random_colors(N)
+    # colors = colors or random_colors(N)
+    colors = []
+    for times in range(N):
+        colors.append((0.0, 0.0, 0.0))
 
     # Show area outside image boundaries.
-    height, width = image.shape[:2]
-    ax.set_ylim(height + 10, -10)
-    ax.set_xlim(-10, width + 10)
-    ax.axis('off')
-    ax.set_title(title)
+    # height, width = image.shape[:2]
+    # ax.set_ylim(height + 10, -10)
+    # ax.set_xlim(-10, width + 10)
+    # ax.axis('off')
+    # ax.set_title(title)
 
     masked_image = image.astype(np.uint32).copy()
     for i in range(N):
         color = colors[i]
 
         # Bounding box
-        if not np.any(boxes[i]):
-            # Skip this instance. Has no bbox. Likely lost in image cropping.
-            continue
-        y1, x1, y2, x2 = boxes[i]
-        if show_bbox:
-            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
-                                alpha=0.7, linestyle="dashed",
-                                edgecolor=color, facecolor='none')
-            ax.add_patch(p)
+        # if not np.any(boxes[i]):
+        #     # Skip this instance. Has no bbox. Likely lost in image cropping.
+        #     continue
+        # y1, x1, y2, x2 = boxes[i]
+        # if show_bbox:
+        #     p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+        #                         alpha=0.7, linestyle="dashed",
+        #                         edgecolor=color, facecolor='none')
+        #     ax.add_patch(p)
 
         # Label
-        if not captions:
-            class_id = class_ids[i]
-            score = scores[i] if scores is not None else None
-            label = class_names[class_id]
-            caption = "{} {:.3f}".format(label, score) if score else label
-        else:
-            caption = captions[i]
-        ax.text(x1, y1 + 8, caption,
-                color='w', size=11, backgroundcolor="none")
+        # if not captions:
+        #     class_id = class_ids[i]
+        #     score = scores[i] if scores is not None else None
+        #     label = class_names[class_id]
+        #     caption = "{} {:.3f}".format(label, score) if score else label
+        # else:
+        #     caption = captions[i]
+        # ax.text(x1, y1 + 8, caption,
+        #         color='w', size=11, backgroundcolor="none")
+
+        class_id = class_ids[i]
+        label = class_names[class_id]
 
         # Mask
         mask = masks[:, :, i]
-        if show_mask:
+        if show_mask and label == 'person':
             masked_image = apply_mask(masked_image, mask, color)
 
         # Mask Polygon
         # Pad to ensure proper polygons for masks that touch image edges.
-        padded_mask = np.zeros(
-            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
-        padded_mask[1:-1, 1:-1] = mask
-        contours = find_contours(padded_mask, 0.5)
-        for verts in contours:
-            # Subtract the padding and flip (y, x) to (x, y)
-            verts = np.fliplr(verts) - 1
-            p = Polygon(verts, facecolor="none", edgecolor=color)
-            ax.add_patch(p)
-    ax.imshow(masked_image.astype(np.uint8))
-    if auto_show:
-        plt.show()
+        # padded_mask = np.zeros(
+        #     (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        # padded_mask[1:-1, 1:-1] = mask
+        # contours = find_contours(padded_mask, 0.5)
+        # for verts in contours:
+        #     # Subtract the padding and flip (y, x) to (x, y)
+        #     verts = np.fliplr(verts) - 1
+        #     p = Polygon(verts, facecolor="none", edgecolor=color)
+        #     ax.add_patch(p)
+    # ax.imshow(masked_image.astype(np.uint8))
+    # if auto_show:
+    # plt.show()
+    return masked_image.astype(np.uint8)
+
 
 
 def display_differences(image,
@@ -498,3 +506,4 @@ def display_weight_stats(model):
                 "{:+9.4f}".format(w.std()),
             ])
     display_table(table)
+
